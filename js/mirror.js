f662df89cd7d459564c9aaeb316eedde59abf94b
stanislavstashchuk <stashchuk_stanislav@ukr.net>
2021-08-30 00:10:26 +0300

    initial

--- js/mirror.js
+++ js/mirror.js
@@ -4,3 +4,32 @@ let sidebar = document.querySelector("#mirror-sidebar");
 btn.onclick = function() {
     sidebar.classList.toggle("act");
 }
+
+$(document).ready(function(){
+    $('.mirror-gallary__slader').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        mobileFirst: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '50px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '50px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+  });
