a45ebdfc936e2d18ea433fcd1c0a305decac4a6d
stanislavstashchuk <stashchuk_stanislav@ukr.net>
2021-08-30 18:05:39 +0300

    initial

--- js/closets.js
+++ js/closets.js
@@ -4,3 +4,32 @@ let sidebar = document.querySelector(".closets-sidebar");
 btn.onclick = function() {
     sidebar.classList.toggle("active");
 }
+
+$(document).ready(function(){
+    $('.closets-gallary__slader').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        mobileFirst: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '50px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '50px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+  });
